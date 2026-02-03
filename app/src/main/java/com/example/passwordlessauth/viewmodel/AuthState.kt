c18c572d7a15eb55d2aa785fbda867f19eab5afa
ASHUTOSH PAROUHA <ashuparouha2004@gmail.com>
2026-02-04 01:35:55 +0530

    feat: add authentication UI state model

--- app/src/main/java/com/example/passwordlessauth/viewmodel/AuthState.kt
+++ app/src/main/java/com/example/passwordlessauth/viewmodel/AuthState.kt
@@ -1,4 +1,9 @@
 package com.example.passwordlessauth.viewmodel
 
-class AuthState {
-}
+data class AuthState(
+    val email: String = "",
+    val isOtpSent: Boolean = false,
+    val isLoggedIn: Boolean = false,
+    val error: String? = null,
+    val sessionStartTime: Long? = null
+)
